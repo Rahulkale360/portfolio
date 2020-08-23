d610abf4a7012591d5836461b11dfde701b00699
Rahulkale360 <33682849+Rahulkale360@users.noreply.github.com>
2020-08-23 23:08:34 +0530

    Create sever.js

---
+++ sever.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/dist/portfolio'));
+
+//PathLocationStrategy
+app.get('/*',function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'))
+})
+
+app.listen(process.env.PORT || 8080);
