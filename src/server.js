4fa814a66ed96fc48c82f5b6f14e142dd34a3b23
unknown <rahulkale360@gmail.com>
2020-08-24 09:36:44 +0530

    removed favicon

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/portfolio'));
 
 //PathLocationStrategy
 app.get('/*',function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'))
 })
 
 app.listen(process.env.PORT || 8080);
